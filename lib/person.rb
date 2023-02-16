0c4f1361a5acf96b3b70f6b1566738b1e78c3466
Loise <loise.wambui@student.moringaschool.com>
2023-02-17 01:28:51 +0300

    commit tree b8218b682b698dc39fa0a544364e64614982fd23

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-  # your code here
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=" ,value)
+    end
+  end
 end
+
+bob_attributes = {name: "Bob", height: 100}
+bob = Person.new(bob_attributes)
+puts bob.name
+puts bob.height
+
+susan_attributes = {name: "Susan", eye_color: "red", height: 50}
+susan = Person.new(susan_attributes)
+puts susan.name
+puts susan.eye_color
+puts susan.height
